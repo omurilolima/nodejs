bf5f0c7a31d08307cb1d5eded681cf4285c4a363
omurilolima <msl@cesar.school>
2025-07-16 19:07:22 +0100

    Path Module

--- first-app/app.js
+++ first-app/app.js
@@ -1,3 +1,6 @@
-const log = require("./logger");
+const path = require("path");
+// The path.parse() method returns an object whose properties represent significant elements of the path.
+// more at https://nodejs.org/docs/latest/api/path.html#pathparsepath
+var pathObj = path.parse(__filename);
 
-log("message");
+console.log(pathObj);
