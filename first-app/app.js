485bc1423f79968c384935d855d2ea307229929d
omurilolima <msl@cesar.school>
2025-07-17 16:50:28 +0100

    HTTP Module

--- first-app/app.js
+++ first-app/app.js
@@ -1,9 +1,22 @@
-const Logger = require("./logger");
-const logger = new Logger();
+// HTTP Module
+// More at https://nodejs.org/docs/latest/api/http.html
 
-// Register a listener
-logger.on("messageLogged", (arg) => {
-	console.log("Listerner called", arg);
+const http = require("http");
+
+const server = http.createServer((request, response) => {
+	if (request.url === "/") {
+		response.write("Hello World");
+		response.end();
+	}
+
+	if (request.url === "/api/courses") {
+		response.write(JSON.stringify([1, 2, 3]));
+		response.end();
+	}
 });
 
-logger.log("message");
+server.listen(3000);
+
+console.log("Listening on port 3000...");
+// on browser access: localhost:3000 to see the "Hello World"
+// on browser access: localhost:3000/api/courses to see the JSON to string
