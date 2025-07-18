9004bbe32b7aa5682dcc682f830daffaa78f5a71
omurilolima <msl@cesar.school>
2025-07-18 19:52:20 +0100

    My first web server with Express

---
+++ express-demo/index.js
@@ -0,0 +1,14 @@
+// Express documentation: https://expressjs.com/en/5x/api.html
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+	res.send("Hello World");
+});
+
+app.get("/api/courses", (req, res) => {
+	res.send([1, 2, 3]);
+});
+
+app.listen(3000, () => console.log("Listening on port 3000..."));
