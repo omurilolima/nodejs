b27c45dececfffc8e3a332320ce0271b8137e3a2
omurilolima <msl@cesar.school>
2025-07-18 16:11:23 +0100

    Install all the dependencies

--- npm-demo/index.js
+++ npm-demo/index.js
@@ -4,3 +4,9 @@ var result = _.contains([1, 2, 3, 4], 2);
 console.log(result);
 
 // For more packages, acess: https://www.npmjs.com/
+
+// As the size o the node_modules folder can grow significantly.
+// We should always list the node_modules folder in the .gitignore file
+// To install all the dependencies of an existing project, run: "npm i"
+//  With this, npm looks up our package.json file and download those
+//  dependencies from npm registry.
