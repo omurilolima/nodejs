5c3aad5c0838e00c3e8b78ff4d3c4dd59bdfa8fd
omurilolima <msl@cesar.school>
2025-07-21 15:18:44 +0100

    Route and Query String parameters

--- express-demo/index.js
+++ express-demo/index.js
@@ -11,5 +11,20 @@ app.get("/api/courses", (req, res) => {
 	res.send([1, 2, 3]);
 });
 
+app.get("/api/courses/:id", (req, res) => {
+	res.send(req.params.id);
+});
+
+// Use route parameters (e.g. posts/2018/1) for essential or required values
+// Use query string parameters (e.g. ?sortBy=name) for anything that is optional
+
+app.get("/api/posts/:year/:month", (req, res) => {
+	// For getting route parameters
+	// res.send(req.params);
+
+	// For getting query string parameters
+	res.send(req.query);
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
