1fa2e7676495daae01bca66289094d85ba17273c
omurilolima <msl@cesar.school>
2025-07-23 18:09:45 +0100

    Handling HTTP DELETE requests

--- express-demo/index.js
+++ express-demo/index.js
@@ -25,16 +25,13 @@ app.get("/api/courses", (req, res) => {
 app.get("/api/courses/:id", (req, res) => {
 	const course = courses.find((c) => c.id === parseInt(req.params.id));
 	if (!course)
-		res.status(404).send("The course with the given ID was not found");
+		return res.status(404).send("The course with the given ID was not found");
 	res.send(course);
 });
 
 app.post("/api/courses", (req, res) => {
 	const { error } = validateCourse(req.body);
-	if (error) {
-		res.status(400).send(error.details[0].message);
-		return;
-	}
+	if (error) return res.status(400).send(error.details[0].message);
 
 	const course = {
 		id: courses.length + 1,
@@ -49,18 +46,30 @@ app.put("/api/courses/:id", (req, res) => {
 	const course = courses.find((c) => c.id === parseInt(req.params.id));
 	// If not existing, return 404
 	if (!course)
-		res.status(404).send("The course with the given ID was not found");
+		return res.status(404).send("The course with the given ID was not found");
 
 	// Validate the course
 	const { error } = validateCourse(req.body);
 	// If invalid, return 400 - Bad request
-	if (error) {
-		res.status(400).send(error.details[0].message);
-		return;
-	}
+	if (error) return res.status(400).send(error.details[0].message);
 	// Update course
 	course.name = req.body.name;
 	// Return the updated course
+	res.send(course);
+});
+
+app.delete("/api/courses/:id", (req, res) => {
+	// Look up the course
+	const course = courses.find((c) => c.id === parseInt(req.params.id));
+	// If not existing, return 404
+	if (!course)
+		return res.status(404).send("The course with the given ID was not found");
+
+	// Delete
+	const index = courses.indexOf(course);
+	courses.splice(index, 1);
+
+	// Return the same course
 	res.send(course);
 });
 
