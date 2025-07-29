2e76cb5692ae6bd8023a18bc66eb8fd6dcd25aad
omurilolima <msl@cesar.school>
2025-07-29 16:19:27 +0100

    Consuming Promises

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,11 +1,18 @@
 console.log("Before");
-getUser(1, (user) => {
-	getRepositories(user.gitHubUsername, (repos) => {
-		getCommits(repos[0], (commits) => {
-			console.log(commits);
-		});
-	});
-});
+// getUser(1, (user) => {
+// 	getRepositories(user.gitHubUsername, (repos) => {
+// 		getCommits(repos[0], (commits) => {
+// 			console.log(commits);
+// 		});
+// 	});
+// });
+
+getUser(1)
+	.then((user) => getRepositories(user.gitHubUsername))
+	.then((repos) => getCommits(repos[0]))
+	.then((commits) => console.log("Commits", commits))
+	.catch((err) => console.log("Error", err.message));
+
 console.log("After");
 
 function getUser(id) {
@@ -20,7 +27,7 @@ function getUser(id) {
 function getRepositories(username) {
 	return new Promise((resolve, reject) => {
 		setTimeout(() => {
-			console.log("Calling GitHub API...");
+			console.log("Calling GitHub API to get Repos...");
 			resolve(["repo1", "repo2", "repo3"]);
 		}, 2000);
 	});
@@ -29,8 +36,8 @@ function getRepositories(username) {
 function getCommits(repo) {
 	return new Promise((resolve, reject) => {
 		setTimeout(() => {
-			console.log("Calling GitHub API...");
-			resolve(["commit"]);
+			console.log("Calling GitHub API to get Commits...");
+			resolve(["commit1", "commit2", "commit3"]);
 		}, 2000);
 	});
 }
