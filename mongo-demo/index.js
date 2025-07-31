59f9d2a6575b6c864058519ffb126d482d84333f
omurilolima <msl@cesar.school>
2025-07-31 16:21:09 +0100

    Schemas, Models, Saving and Querying

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -4,3 +4,38 @@ mongoose
 	.connect("mongodb://localhost/playground")
 	.then(() => console.log("Connected to MongoDB..."))
 	.catch((err) => console.log("Could not connect to MongoDB...", err));
+
+// SCHEMA (It is specific to Mongoose, it's not part of MongoDB)
+// We use a schema in Mangoose to define the shape of documents in a MongoDB collection.
+
+const courseSchema = new mongoose.Schema({
+	name: String,
+	author: String,
+	tags: [String],
+	date: { type: Date, default: Date.now },
+	isPublished: Boolean,
+});
+
+const Course = mongoose.model("Course", courseSchema);
+
+async function createCourse() {
+	const course = new Course({
+		name: "Angular Course",
+		author: "Mosh",
+		tags: ["angular", "backend"],
+		isPublished: true,
+	});
+
+	const result = await course.save();
+	console.log(result);
+}
+
+async function getCourses() {
+	const courses = await Course.find({ author: "Mosh", isPublished: true })
+		.limit(10)
+		.sort({ name: 1 })
+		.select({ name: 1, tags: 1 });
+	console.log(courses);
+}
+
+getCourses();
